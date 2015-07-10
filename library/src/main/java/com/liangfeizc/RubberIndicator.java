06268a28effac3bb39bd4accfc45012e40b1dd3a
liangfeizc <njliangfei@gmail.com>
2015-07-11 00:12:09 +0800

    change some local variables to be members for efficency

--- library/src/main/java/com/liangfeizc/RubberIndicator.java
+++ library/src/main/java/com/liangfeizc/RubberIndicator.java
@@ -17,6 +17,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -34,10 +35,11 @@ public class RubberIndicator extends RelativeLayout {
     private static final int LARGE_CIRCLE_RADIUS = 25;
     private static final int OUTER_CIRCLE_RADIUS = 50;
 
+    private static final int BEZIER_CURVE_ANCHOR_DISTANCE = 30;
+
     private static final int CIRCLE_TYPE_SMALL = 0x00;
     private static final int CIRCLE_TYPE_LARGE = 0x01;
     private static final int CIRCLE_TYPE_OUTER = 0x02;
-
 
     /**
      * colors
@@ -60,19 +62,27 @@ public class RubberIndicator extends RelativeLayout {
     private CircleView mLargeCircle;
     private CircleView mSmallCircle;
     private CircleView mOuterCircle;
-    private CircleView[] mSmallCircleViews;
+    private CircleView[] mSmallCircles;
 
     /**
      * animations
      */
     private AnimatorSet mAnim;
-    private PropertyValuesHolder pvhScaleX;
-    private PropertyValuesHolder pvhScaleY;
-    private PropertyValuesHolder pvhRotation;
-    private PropertyValuesHolder pvhScale;
-    private PropertyValuesHolder mPvhShake;
+    private PropertyValuesHolder mPvhScaleX;
+    private PropertyValuesHolder mPvhScaleY;
+    private PropertyValuesHolder mPvhScale;
+    private PropertyValuesHolder mPvhRotation;
 
+    /**
+     * Movement Path
+     */
+    private Path mSmallCirclePath;
+
+    /**
+     * Helper values
+     */
     private int mSmallCircleIndex = 0;
+    private int mBezierCurveAnchorDistance = dp2px(BEZIER_CURVE_ANCHOR_DISTANCE);
     private boolean mDown = true;
 
     public RubberIndicator(Context context) {
@@ -100,11 +110,12 @@ public class RubberIndicator extends RelativeLayout {
         mLargeCircleRadius = dp2px(LARGE_CIRCLE_RADIUS);
         mOuterCircleRadius = dp2px(OUTER_CIRCLE_RADIUS);
 
-        mAnim = new AnimatorSet();
-        pvhScaleX = PropertyValuesHolder.ofFloat("scaleX", 1, 0.8f, 1);
-        pvhScaleY = PropertyValuesHolder.ofFloat("scaleY", 1, 0.8f, 1);
-        pvhScale = PropertyValuesHolder.ofFloat("scaleY", 1, 0.5f, 1);
+        mPvhScaleX = PropertyValuesHolder.ofFloat("scaleX", 1, 0.8f, 1);
+        mPvhScaleY = PropertyValuesHolder.ofFloat("scaleY", 1, 0.8f, 1);
+        mPvhScale = PropertyValuesHolder.ofFloat("scaleY", 1, 0.5f, 1);
+        mPvhRotation = PropertyValuesHolder.ofFloat("rotation", 0);
 
+        mSmallCirclePath = new Path();
     }
 
     @Override
@@ -130,11 +141,11 @@ public class RubberIndicator extends RelativeLayout {
         mContainer.addView(mLargeCircle);
 
         int size = count - 1;
-        mSmallCircleViews = new CircleView[size];
+        mSmallCircles = new CircleView[size];
         for (int i = 0; i < size; i++) {
             CircleView circleView = createCircleView(CIRCLE_TYPE_SMALL);
             mContainer.addView(circleView);
-            mSmallCircleViews[i] = circleView;
+            mSmallCircles[i] = circleView;
         }
     }
 
@@ -168,42 +179,43 @@ public class RubberIndicator extends RelativeLayout {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void move(final boolean down) {
-        mSmallCircle = mSmallCircleViews[mSmallCircleIndex];
+        mSmallCircle = mSmallCircles[mSmallCircleIndex];
 
+        // Calculate the new x coordinate for circles.
         float smallCircleX = down ? mLargeCircle.getX()
                 : mLargeCircle.getX() + mLargeCircle.getWidth() - mSmallCircle.getWidth();
         float largeCircleX = down ?
                 mSmallCircle.getX() + mSmallCircle.getWidth() - mLargeCircle.getWidth() : mSmallCircle.getX();
         float outerCircleX = mOuterCircle.getX() + largeCircleX - mLargeCircle.getX();
 
-        // animations for large circle and outer circle
+        // animations for large circle and outer circle.
         PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat("x", mLargeCircle.getX(), largeCircleX);
         ObjectAnimator largeCircleAnim = ObjectAnimator.ofPropertyValuesHolder(
-                mLargeCircle, pvhX, pvhScaleX, pvhScaleY);
+                mLargeCircle, pvhX, mPvhScaleX, mPvhScaleY);
 
         pvhX = PropertyValuesHolder.ofFloat("x", mOuterCircle.getX(), outerCircleX);
         ObjectAnimator outerCircleAnim = ObjectAnimator.ofPropertyValuesHolder(
-                mOuterCircle, pvhX, pvhScaleX, pvhScaleY);
+                mOuterCircle, pvhX, mPvhScaleX, mPvhScaleY);
 
         // Animations for small circle
         PointF smallCircleCenter = mSmallCircle.getCenter();
         PointF smallCircleEndCenter = new PointF(
                 smallCircleCenter.x - (mSmallCircle.getX() - smallCircleX), smallCircleCenter.y);
 
-        Path motionPath = new Path();
-        motionPath.moveTo(smallCircleCenter.x, smallCircleCenter.y);
-        motionPath.quadTo(smallCircleCenter.x, smallCircleCenter.y,
+        // Create motion anim for small circle.
+        mSmallCirclePath.reset();
+        mSmallCirclePath.moveTo(smallCircleCenter.x, smallCircleCenter.y);
+        mSmallCirclePath.quadTo(smallCircleCenter.x, smallCircleCenter.y,
                 (smallCircleCenter.x + smallCircleEndCenter.x) / 2,
-                (smallCircleCenter.y + smallCircleEndCenter.y) / 2 + 80);
-        motionPath.lineTo(smallCircleEndCenter.x, smallCircleEndCenter.y);
+                (smallCircleCenter.y + smallCircleEndCenter.y) / 2 + mBezierCurveAnchorDistance);
+        mSmallCirclePath.lineTo(smallCircleEndCenter.x, smallCircleEndCenter.y);
 
-        final PathMeasure pathMeasure = new PathMeasure(motionPath, false);
-
-        ValueAnimator smallCircleAnim = null;
+        ValueAnimator smallCircleAnim;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            smallCircleAnim = ObjectAnimator.ofObject(mSmallCircle, "center", null, motionPath);
+            smallCircleAnim = ObjectAnimator.ofObject(mSmallCircle, "center", null, mSmallCirclePath);
 
         } else {
+            final PathMeasure pathMeasure = new PathMeasure(mSmallCirclePath, false);
             final float[] point = new float[2];
             smallCircleAnim = ValueAnimator.ofFloat(0.0f, 1.0f);
             smallCircleAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -216,8 +228,12 @@ public class RubberIndicator extends RelativeLayout {
             });
         }
 
-        pvhRotation = PropertyValuesHolder.ofFloat("rotation", 0, mDown ? -30f : 30f, 0, mDown ? 30f : -30f, 0);
-        ObjectAnimator otherAnim = ObjectAnimator.ofPropertyValuesHolder(mSmallCircle, pvhRotation, pvhScale);
+        //PropertyValuesHolder pvhRotation = PropertyValuesHolder.ofFloat("rotation", 0,
+        //        mDown ? -30f : 30f, 0, mDown ? 30f : -30f, 0);
+
+        mPvhRotation.setFloatValues(0, mDown ? -30f : 30f, 0, mDown ? 30f : -30f, 0);
+        ObjectAnimator otherAnim = ObjectAnimator.ofPropertyValuesHolder(mSmallCircle, mPvhRotation, mPvhScale);
+
 
         //float y = mSmallCircle.getY();
         //PropertyValuesHolder pvhShake = PropertyValuesHolder.ofFloat("y", y, y - 10, y);
@@ -238,18 +254,16 @@ public class RubberIndicator extends RelativeLayout {
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                mSmallCircle.setX(Math.round(mSmallCircle.getX() + 0.5));
-                mSmallCircle.setY(Math.round(mSmallCircle.getY() + 0.5));
+                //mSmallCircle.setX(Math.round(mSmallCircle.getX() + 0.5));
+                //mSmallCircle.setY(Math.round(mSmallCircle.getY() + 0.5));
 
-                if (mDown && mSmallCircleIndex == mSmallCircleViews.length - 1) {
+                if (mDown && mSmallCircleIndex == mSmallCircles.length - 1) {
                     mDown = false;
                 } else if (!mDown && mSmallCircleIndex == 0) {
                     mDown = true;
                 } else {
                     mSmallCircleIndex = mSmallCircleIndex + (mDown ? 1 : -1);
                 }
-
-
             }
 
             @Override
@@ -270,7 +284,7 @@ public class RubberIndicator extends RelativeLayout {
     }
 
     public void move() {
-        if (mAnim.isRunning()) {
+        if (mAnim != null && mAnim.isRunning()) {
             return;
         }
         move(mDown);

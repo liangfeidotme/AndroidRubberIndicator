f5dc3608e7681fdaa89f6338863276df5dfc8032
Hugo Gresse <hugo.gresse@teads.tv>
2015-09-10 16:22:14 +0200

    Prevent setCount for adding more view each times by clearing container and collections

--- library/src/main/java/com/liangfeizc/RubberIndicator.java
+++ library/src/main/java/com/liangfeizc/RubberIndicator.java
@@ -189,13 +189,21 @@ public class RubberIndicator extends RelativeLayout {
             throw new IllegalArgumentException("focus position must be less than count");
         }
 
-        int i = 0;
-        for (; i < focusPos; i++) {
-            addSmallCircle();
-        }
-        addLargeCircle();
-        for (i = focusPos + 1; i < count; i++) {
-            addSmallCircle();
+        /* Check if the number on indicator has changed since the last setCount to prevent duplicate */
+        if(mCircleViews.size() != count) {
+            mContainer.removeAllViews();
+            mCircleViews.clear();
+
+            int i = 0;
+            for (; i < focusPos; i++) {
+                addSmallCircle();
+            }
+
+            addLargeCircle();
+
+            for (i = focusPos + 1; i < count; i++) {
+                addSmallCircle();
+            }
         }
 
         mFocusPosition = focusPos;
